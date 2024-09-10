6e1de396d3d0708b1532388194ce44a79c1cd68a
Limbu <limbureubenn@gmail.com>
2024-09-10 10:27:33 +0300

    creations of service file

--- frontend/src/app/user/sign/sign.component.ts
+++ frontend/src/app/user/sign/sign.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { userInfo } from 'os';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-sign',
@@ -8,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrl: './sign.component.scss'
 })
 export class SignComponent {
+
+formData = new FormGroup({
+username: new FormControl('', Validators.required),
+email: new FormControl('', [Validators.required, Validators.email]),
+password: new FormControl('', Validators.required)
+});
+
+username: string = '';
+email: string = '';
+password: string = '';
+registrationError: string = '';
+model: any;
+
+constructor(private authService: AuthService, private router: Router) {
+
+}
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  if (this.authService.isLoggedIn()) {
+    this.router.navigate(['/login']);
+  }
+}
 
 }
